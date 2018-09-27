3790f03fd63531d7caa0aefb000bb2be72e356e0
user <user@DESKTOP-7EQF3M8>
2018-09-27 21:52:28 +0900

    1

--- Pf_project01/src/Gameserver/Gaming.java
+++ Pf_project01/src/Gameserver/Gaming.java
@@ -1,6 +1,7 @@
 package Gameserver;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Gaming implements Serializable {
 	/**
@@ -11,14 +12,36 @@ public class Gaming implements Serializable {
 	public static final int GAME_READY = 2;
 	public static final int GAME_DIE = 10;
 	public static final int GAME_GO = 11;
+	
+	
+	private int who;
 	private int what;
-	public Gaming(int n) {
-		setWhat(n);
+	
+	private List<Player> players;
+	
+	public Gaming(int what) {
+		setWhat(what);
 	}
+	public Gaming(int who, int what, List<Player> players) {
+		setWho(who); setWhat(what); setPlayers(players);
+	}
+	
 	public int getWhat() {
 		return what;
 	}
 	public void setWhat(int what) {
 		this.what = what;
+	}
+	public int getWho() {
+		return who;
+	}
+	public void setWho(int who) {
+		this.who = who;
+	}
+	public List<Player> getPlayers() {
+		return players;
+	}
+	public void setPlayers(List<Player> players) {
+		this.players = players;
 	}
 }
